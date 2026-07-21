028e0ce4c380146da5a6d119002cf546e0044044
Matheus Silvério <mattdavm@hotmail.com>
2026-07-20 21:36:01 -0300

    remove part of code

--- CameraFile.cs
+++ CameraFile.cs
@@ -19,33 +19,13 @@ namespace Bagel
         {
             get => m_MainMenuCamera.Lens.FieldOfView;
             set => UpdateFieldOfView(value);
-        }
-
-        void Awake()
-        {
-            m_State = m_PlayManager.state;
-            m_State.onStateChange += (_, _) => UpdateCamera();
-            m_State.onMainMenuPaneModeChange += (_, _) => UpdateCamera();
-        }
-
-        void OnEnable()
-        {
-            UpdateCamera();
-        }
+        } 
 
         void UpdateFieldOfView(float newFOV)
         { 
             m_MainMenuSecondaryCamera.Lens.FieldOfView = newFOV;
             m_PlayCamera.Lens.FieldOfView = newFOV;
         }
-
-        void UpdateCamera()
-        {
-            m_MainMenuCamera.enabled = m_State.currentState == PlayManagerState.State.MainMenu && m_State.currentMainMenuPaneMode == PlayManagerState.MainMenuPaneMode.Primary;
-            m_MainMenuSecondaryCamera.enabled = m_State.currentState == PlayManagerState.State.MainMenu && m_State.currentMainMenuPaneMode == PlayManagerState.MainMenuPaneMode.Secondary;
-            m_BagelSelectionCamera.enabled = m_State.currentState == PlayManagerState.State.BagelSelection;
-            m_PlayCamera.enabled = m_State.currentState == PlayManagerState.State.Playing;
-            m_GameOverCamera.enabled = m_State.currentState == PlayManagerState.State.GameOver;
-        }
+ 
     }
 }
